248e79a03528cbc7e8c9871743ad368a77245005
Ykykand1 <andikojanaj321@gmail.com>
2026-04-17 19:56:34 +0200

    Changed delete button text from "Fshi" to "FSHIJ"

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addTask() {
   li.textContent = taskText;
 
   const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Fshi";
+  deleteBtn.textContent = "FSHIJ";
   deleteBtn.className = "deleteBtn";
   deleteBtn.onclick = () => li.remove();
 
